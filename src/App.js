86aeb6f6ced0cba7d9766145f005ae53b1c01b02
Matthew Mogavero <matt.moga@gmail.com>
2018-02-06 20:18:18 -0500

    rewrote the function to use named functions for the click headers

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
     this.state = {
       count: 0
     }
+    this.increment = this.increment.bind(this); //binding means inside of "increment"                                                //the "this" keyword is the component
+    this.decrement = this.decrement.bind(this);
+  }
+  increment(){
+    this.setState({count: this.state.count + 1})
+  }
+  decrement(){
+    this.setState({count: this.state.count - 1})
   }
   render() {
     // you can return only one HTML element per render, hence <div>
     return (
      <div>
-       <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
-       <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
+       <button onClick={this.increment}>+</button>
+       <button onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
      </div>
     );
