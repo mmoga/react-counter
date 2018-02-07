d85180d681a757899e07b785943a8e1261dfdcf9
Matthew Mogavero <matt.moga@gmail.com>
2018-02-06 19:48:59 -0500

    made the incremeent and decrement work on a counter. Additionally, created the layout for a counter and added files to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
   render() {
+    // you can return only one HTML element per render, hence <div>
     return (
-      <div>
-        <h1>Hello</h1>
-      <h2>Something else</h2>
-      </div>
+     <div>
+       <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
+       <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
+       <h1>{this.state.count}</h1>
+     </div>
     );
   }
 }
