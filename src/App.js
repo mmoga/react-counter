8a8d0445865fc1dd48120f0fd7fd466dbb9e3bbb
Matthew Mogavero <matt.moga@gmail.com>
2018-02-06 21:08:59 -0500

    added the ability to set a value to increment/decrement

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      count: 0,
+      value: 0
     }
     this.increment = this.increment.bind(this); //binding means inside of "increment"                                                //the "this" keyword is the component
     this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
   }
   increment(){
-    this.setState({count: this.state.count + 1})
+    this.setState({count: this.state.count + this.state.value})
   }
   decrement(){
-    this.setState({count: this.state.count - 1})
+    this.setState({count: this.state.count - this.state.value})
+  }
+  reset(){
+    this.setState({count: 0})
+  }
+  handleValueChange(e){
+    this.setState({value: +e.target.value})
   }
   render() {
     // you can return only one HTML element per render, hence <div>
     return (
      <div>
+       <label>
+         Value:
+       <input type="number" name="" id="" value={this.state.value} onChange={this.handleValueChange} />
+       </label>
        <button onClick={this.increment}>+</button>
        <button onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
+       <button onClick={this.reset}>Reset</button>
      </div>
     );
   }
