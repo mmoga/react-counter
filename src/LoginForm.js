00ba4b8e61f019680a232631faa4d7ee80d3f785
Matthew Mogavero <matt.moga@gmail.com>
2018-02-07 20:48:33 -0500

    added checkbox to form in LoginForm

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -14,6 +14,10 @@ class LoginForm extends Component {
                     <label for="password"></label>
                     <input type="password" className="form-control" placeholder="Password"/>
                 </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                     <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+                </div>
                 <button className="btn btn-lg btn-primary" onClick={() => alert('Hell yeah!')}>Log in</button>
                 </form>
             </div>
