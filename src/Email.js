651eb73cdb2af0586b36eaff24a2bd46188520d0
Matthew Mogavero <matt.moga@gmail.com>
2018-02-07 20:23:01 -0500

    added a pinch of style to the email component

--- src/Email.js
+++ src/Email.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Email extends Component {
     render(){
         return (
-            <h1>Email!</h1>
+            <div className="jumbotron">
+                <h1>Email</h1>
+                <p>blah@blah.com</p>
+            </div>
         );
     }
 }
